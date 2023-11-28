d369e51ac4593708b55e6110117e94045409624b
dobby2025 <128953898+dobby2025@users.noreply.github.com>
2023-11-28 20:07:33 +0900

    구구단

--- src/section05/LoopHomeWork2.java
+++ src/section05/LoopHomeWork2.java
@@ -31,5 +31,31 @@ package section05;
 8 X 9 = 72	9 X 9 = 81	
 */
 public class LoopHomeWork2 {
-
+	
+	public static void main(String[] args) {
+		for(int k = 0; k < 3; k++) {
+			for(int i = 0; i < 9; i++) {
+				for(int j = 0; j < 3; j++) {
+					int dan = j + 2 + (k*3);
+					if(dan == 10) break;
+					int multiply = i+1;
+					int result = dan * multiply;
+					System.out.printf("%d X %d = %d\t", dan, multiply, result);
+				}
+				System.out.println();
+			}
+			System.out.println();
+		}
+		
+	}
 }
+
+
+
+
+
+
+
+
+
+
