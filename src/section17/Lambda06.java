37186b6a4e6da68d75044caac13f4f5eb8aacdde
dobby2025 <128953898+dobby2025@users.noreply.github.com>
2023-12-20 21:22:33 +0900

    람다람다람다람쥐

--- src/section17/Lambda06.java
+++ src/section17/Lambda06.java
@@ -1,5 +1,61 @@
 package section17;
 
-public class Lambda06 {
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
+
+class EvenNumberPredicate implements Predicate<Integer> {
+	@Override
+	public boolean test(Integer n) {
+		
+		return n % 2 == 0;
+	}
 }
+
+class NumberSquareMapper implements Function<Integer, Integer> {
+	@Override
+	public Integer apply(Integer n) {
+		return n * n;
+	}
+}
+
+class SystemOutConsumer implements Consumer<Integer> {
+	@Override
+	public void accept(Integer n) {
+		System.out.println("n: " + n);
+	}
+}
+
+
+
+
+
+
+public class Lambda06 {
+	
+	public static void main(String[] args) {
+		
+		List<Integer> numbers = List.of(1, 2, 3, 4, 5);
+		
+		numbers.stream()
+			.filter(n -> n % 2 == 0)
+			.map(n -> n * n)
+			.forEach(n -> System.out.println(n));
+		
+		System.out.println("=================================");
+		
+		numbers.stream()
+				.filter(new EvenNumberPredicate())
+				.map(new NumberSquareMapper())
+				.forEach(new SystemOutConsumer());
+	}
+}
+
+
+
+
+
+
+
