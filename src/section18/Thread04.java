fe4797a30b23d9b05e98bc805313e6deb5c3c60c
dobby2025 <128953898+dobby2025@users.noreply.github.com>
2023-12-22 19:40:54 +0900

    interThread

---
+++ src/section18/Thread04.java
@@ -0,0 +1,13 @@
+package section18;
+
+public class Thread04 {
+	
+	public static void main(String[] args) {
+		System.out.println("메인 스레드 시작");
+		
+		
+		
+		System.out.println("메인 스레드 종료");
+	}
+
+}
